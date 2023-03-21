e4b78eb512dc2bcc75e4f55794c3c556c662777b
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-03-21 12:37:40 +0530

    generate api key

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 module.exports = function(){
+    router.get('/genapikey',(req,res,next)=>{
+        res.send(bcrypt.hashSync(process.env.JWTSECRET,8));
+    });
     router.get('',(req,res,next)=>{
         res.send('Welcome to API');
     });
